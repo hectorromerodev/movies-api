168d1610702909230e032f3c9bf68b234e79e9fd
hectorromerodev <hectorromero.developer@gmail.com>
2021-01-06 11:54:03 -0800

    UPD use express parser

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 
+//Import config
 const { config } = require('./config/index');
 
 // Import routes
 const moviesApi = require('./routes/movies');
 
+//Middleware body parser
+app.use(express.json());
+
+// Routes
 moviesApi(app);
 
 
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
 });
+ 
