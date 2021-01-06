7046d7788cf746eb4ded52d9722e924115902a45
hectorromerodev <hectorromero.developer@gmail.com>
2021-01-05 17:24:38 -0800

    UPD routes/movies

--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,7 @@ const { moviesMock } = require('../utils/mocks/movies');
 function moviesApi(app) {
   const router = express.Router();
   app.use('/api/movies', router);
-
+  // Get all
   router.get('/', async function (req, res, next) {
     try{
       const movies = await Promise.resolve(moviesMock);
@@ -16,18 +16,55 @@ function moviesApi(app) {
       next(err);
     }
   });
-
+  // Get one
   router.get('/:moviesId', async function (req, res, next) {
     try {
-      const movies = await Promise.resolve(moviesMock);
+      const movieRetrived = await Promise.resolve(moviesMock[0]);
       res.status(200).json({
-        data: movies,
-        message: 'Movies listed',
+        data: movieRetrived,
+        message: 'Movie retrived',
       });
     } catch (err) {
       next(err);
     }
   });
+  // Add
+  router.post('/', async function (req, res, next) {
+    try {
+      const createdMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(201).json({
+        data: createdMovieId,
+        message: 'Movie created',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+  // Put
+  router.put('/:moviesId', async function (req, res, next) {
+    try {
+      const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: updatedMovieId,
+        message: 'Movie updated',
+      })
+    } catch (err) {
+      next(err);
+    }
+  });
+  // Delete
+  router.delete('/:moviesId', async function (req, res, next) {
+    try {
+      const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: deletedMovieId,
+        message: 'Movie deleted',
+      })
+    } catch (err) {
+      next(err);
+    }
+  });
+
 }
 
 module.exports = moviesApi; 
