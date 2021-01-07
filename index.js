3bd35778c6a9e8599d3e60b655f0e9f643167d8b
hectorromerodev <hectorromero.developer@gmail.com>
2021-01-07 11:05:16 -0800

    UPD index

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const app = express();
 
 //Import config
 const { config } = require('./config/index');
+
 // Import routes
 const moviesApi = require('./routes/movies');
 
-//Middleware body parser
+// Import middleware
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
+
+
+// Middlewares
 app.use(express.json());
 
 // Routes
 moviesApi(app);
 
+// Middleware of error
+app.use(logErrors);
+app.use(errorHandler);
+
+// App start
 app.listen(config.port, () => {
 	console.log(`Listening http://localhost:${config.port}`);
 });
