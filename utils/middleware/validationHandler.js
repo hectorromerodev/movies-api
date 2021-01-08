158bdc54adba58e1e3c64c30579dd5c7129a69b6
hectorromerodev <hectorromero.developer@gmail.com>
2021-01-08 14:10:41 -0800

    UPD Validation handler fixed

--- utils/middleware/validationHandler.js
+++ utils/middleware/validationHandler.js
@@ -1,20 +1,18 @@
 const boom = require('@hapi/boom');
-// const joi = require('joi');
+const joi = require('joi');
 
 function validate(data, schema) {
-	const { error } = schema.validate(data, { errors: { stack: true } });
+	// If schema is not a joi schema convert to a joi schema object otherwise return schema
+	schema = !joi.isSchema(schema) ? joi.object(schema) : schema;
+	const { error } = schema.validate(data);
 	return error;
 }
 
 function validationHandler(schema, data = 'body') {
-	return async (req, res, next) => {
-		try {
-			await schema.validateAsync(req[data]);
-			next();
-		} catch (err) {
-			next(boom.badRequest(err));
-		}
+	return function (req, res, next) {
+		const error = validate(req[data], schema);
+		error ? next(boom.badRequest(error)) : next();
 	};
 }
 
-module.export = validationHandler;
+module.exports = validationHandler;
